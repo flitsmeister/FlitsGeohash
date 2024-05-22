f0292b331661cdc9f2bce2445df76d4aa7683712
Maarten Zonneveld <maartenzonneveld@live.nl>
2024-05-22 12:04:16 +0200

    enum

--- Sources/FlitsGeohash/LengthedGeohash.swift
+++ Sources/FlitsGeohash/LengthedGeohash.swift
@@ -11,37 +11,37 @@ public protocol GeohashLengthed {
     static var length: UInt32 { get }
 }
 
-public struct GeohashLength11: GeohashLengthed {
+public enum GeohashLength11: GeohashLengthed {
     public static let length: UInt32 = 11
 }
-public struct GeohashLength10: GeohashLengthed {
+public enum GeohashLength10: GeohashLengthed {
     public static let length: UInt32 = 10
 }
-public struct GeohashLength9: GeohashLengthed {
+public enum GeohashLength9: GeohashLengthed {
     public static let length: UInt32 = 9
 }
-public struct GeohashLength8: GeohashLengthed {
+public enum GeohashLength8: GeohashLengthed {
     public static let length: UInt32 = 8
 }
-public struct GeohashLength7: GeohashLengthed {
+public enum GeohashLength7: GeohashLengthed {
     public static let length: UInt32 = 7
 }
-public struct GeohashLength6: GeohashLengthed {
+public enum GeohashLength6: GeohashLengthed {
     public static let length: UInt32 = 6
 }
-public struct GeohashLength5: GeohashLengthed {
+public enum GeohashLength5: GeohashLengthed {
     public static let length: UInt32 = 5
 }
-public struct GeohashLength4: GeohashLengthed {
+public enum GeohashLength4: GeohashLengthed {
     public static let length: UInt32 = 4
 }
-public struct GeohashLength3: GeohashLengthed {
+public enum GeohashLength3: GeohashLengthed {
     public static let length: UInt32 = 3
 }
-public struct GeohashLength2: GeohashLengthed {
+public enum GeohashLength2: GeohashLengthed {
     public static let length: UInt32 = 2
 }
-public struct GeohashLength1: GeohashLengthed {
+public enum GeohashLength1: GeohashLengthed {
     public static let length: UInt32 = 1
 }
 
